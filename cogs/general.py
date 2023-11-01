02ae8810fdb252a0993523b5d17aa7b7d56fe019
Hsu Hsiu Wei <df405102@gmail.com>
2023-11-01 23:36:12 +0800

    cogs: add a simple cog as first attempt

---
+++ cogs/general.py
@@ -0,0 +1,27 @@
+import inspect
+import random
+from itertools import cycle
+
+from discord import ApplicationContext
+from ezcord.internal.dc import discord as dc
+from ezcord import Bot, Cog, emb
+
+
+class General(Cog):
+    def __init__(self, bot: Bot):
+        super().__init__(bot)
+
+    @dc.slash_command(
+        name="ping", description="Check if the bot is alive")
+    async def ping(self, ctx: ApplicationContext) -> None:
+        """
+        Check if the bot is alive.
+
+        :param ctx: The ApplicationContext.
+        """
+        msg = f"🏓 Pong!\nThe bot latency is {round(self.bot.latency * 1000)}ms."
+        await emb.success(ctx, msg)
+
+
+def setup(bot: Bot):
+    bot.add_cog(General(bot))
