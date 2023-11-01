aed535327acb85e59e5145e45db7c1d47e4e3381
Hsu Hsiu Wei <df405102@gmail.com>
2023-11-02 01:50:13 +0800

    cogs: add get_members command in db.py

---
+++ cogs/db.py
@@ -0,0 +1,43 @@
+import os
+import json
+import random
+from itertools import cycle
+
+from discord import ApplicationContext
+from ezcord.internal.dc import discord as dc
+from ezcord import Bot, Cog
+from pandas import DataFrame as df
+from table2ascii import table2ascii as t2a
+from table2ascii import PresetStyle
+
+def df_to_ascii_table(df:df) -> str:
+    fields = ["name", "discord_id", "trello_id"]
+    body = [[df[f][i] for f in fields] for i in df.index]
+    table = t2a(
+        header = fields,
+        body = body,
+        style = PresetStyle.thin_compact
+    )
+    return f"```\n{table}\n```"
+
+class Database(Cog):
+    def __init__(self, bot: Bot):
+        super().__init__(bot)
+
+    @dc.slash_command(
+        name="get_members", description="Get member data from database."
+    )
+    async def get_members(self, ctx) -> None:
+        member_list = await self.bot._db.get_member_data(ctx.guild_id)
+        embed = dc.Embed(
+            title = f"{ctx.guild} 的成員們",
+            description = df_to_ascii_table(member_list),
+            color=dc.Colour.fuchsia()
+        )
+
+        await ctx.respond(embed=embed)
+
+
+def setup(bot: Bot):
+    # Uncomment the following line to install this cog.
+    bot.add_cog(Database(bot))
