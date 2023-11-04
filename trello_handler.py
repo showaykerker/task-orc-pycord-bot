dd7e0f6273c75541c62caf28e71d0d4b7310e934
Hsu Hsiu Wei <df405102@gmail.com>
2023-11-05 04:53:42 +0800

    trello: add card classes in trello_handler

--- trello_handler.py
+++ trello_handler.py
@@ -1,7 +1,42 @@
 from typing import Union, List
+import datetime
 
 from trello import TrelloClient
 from trello.board import Board
+from trello.card import Card
+
+class DateCard(Card):
+    def __init__(self, card: Card):
+        self._card = card
+        self.board = card.board_id
+        self.list = card.get_list()
+        self.title = card.name
+        self.members = card.member_id
+        self._due = card.due_date
+
+    @property
+    def due(self):
+        if self._due:
+            return self._due + datetime.timedelta(hours=8)
+        return None
+    
+    def stamp(self) -> float:
+        if self._due:
+            return self._due.timestamp()
+        else:
+            return (datetime.datetime.max - datetime.timedelta(days=1)).timestamp()
+
+
+class FilteredCards:
+    def __init__(self):
+        self._c = []
+
+    def append(self, card: Card):
+        self._c.append(DateCard(card))
+
+    def sort(self):
+        self._c.sort(key=lambda x: x.stamp())
+
 
 
 class TrelloHandler:
