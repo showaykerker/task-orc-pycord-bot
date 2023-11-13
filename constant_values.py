ef8db70a2d2cbeb55afe0812212cdd556f530180
Hsu Hsiu Wei <df405102@gmail.com>
2023-11-14 03:54:24 +0800

    const: add board_emoji

--- constant_values.py
+++ constant_values.py
@@ -1,2 +1,3 @@
 charater_emojis = [":singer:", ":superhero:", ":astronaut:", ":ninja:", ":mx_claus:", ":mage:", ":elf:", ":troll:", ":vampire:", ":zombie:"]
 due_emojis = [":calendar:", ":calendar_spiral:"]
+board_emojis = [":clipboard:"]
