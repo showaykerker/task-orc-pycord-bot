6a201d47a1d1a5510d061860663a4d937d810814
Hsu Hsiu Wei <df405102@gmail.com>
2023-11-23 03:09:11 +0800

    refactor(cogs configuration): re-arrange and make Chinese descriptions

--- cogs/configuration.py
+++ cogs/configuration.py
@@ -2,8 +2,6 @@ import os
 import sys
 import asyncio
 
-import discord
-
 from typing import Dict
 from typing import List
 from typing import Optional
@@ -15,6 +13,8 @@ from discord.ext import commands
 from discord.ext import pages
 from ezcord import Bot
 from ezcord import Cog
+from ezcord import emb
+from ezcord.internal.dc import discord as dc
 
 from discord.commands import guild_only
 from discord.ext.commands import has_any_role
@@ -22,12 +22,16 @@ from discord.ext.commands import has_any_role
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from constant_values import admin_roles
-from modals import BoardKeywordModal
 from trello_handler import get_trello_instance
 from trello_handler import no_trello_error_msg
+from trello_handler import TrelloHandler
+from trello_handler import BoardListData
+from database_handler import TrelloSettings
+from views import ConfigurationPaginator
 from views import SetTrelloBoardListToCreateCardView
 from views import SetTrelloTargetListView
 from views import SetTrelloUserIdView
+from modals import BoardKeywordModal
 
 class Configuration(Cog):
     def __init__(self, bot: Bot):
@@ -49,7 +53,7 @@ class Configuration(Cog):
         await ctx.invoke(self.bot.get_command('db get members'))
 
     @conf_tl.command(
-        name="key_token", description="Set Trello key and token"
+        name="key_token", description="設定 Trello 的密鑰和權杖"
     )
     @guild_only()
     @has_any_role(*admin_roles)
@@ -62,15 +66,51 @@ class Configuration(Cog):
                 await emb.success(ctx, "Trello key and token updated.")
                 self.bot.trello.remove_client(ctx.guild_id)
 
+
     @conf_tl.command(
-        name="member", description="Connect Trello members to Discord membeds"
+        name="member", description="設定 Trello 帳號對應的 Discord 成員"
     )
     @guild_only()
     @has_any_role(*admin_roles)
     async def connect_trello_member(self, ctx: ApplicationContext) -> None:
         await ctx.defer()
-        trello = await get_trello_instance(self, ctx)
+        view = await self._get_set_trello_user_id_view(ctx)
+        await ctx.followup.send("用下拉選單設定成員名稱對照", view=view, embed=view.embed)
 
+    @conf_tl.command(
+        name="list_to_trace", description="設定要追蹤的 Trello 看板"
+    )
+    @guild_only()
+    @has_any_role(*admin_roles)
+    async def set_trello_intersted_lists(self, ctx: ApplicationContext) -> None:
+        await ctx.defer()
+        view = await self._get_set_trello_intersted_lists_view(ctx)
+        await ctx.followup.send("用下拉選單設定追蹤的看板", view=view, embed=view.embed)
+
+    @conf_tl.command(
+        name="board_entry", description="設定用來新增卡片的 Trello 清單"
+    )
+    @guild_only()
+    @has_any_role(*admin_roles)
+    async def set_trello_board_entry(self, ctx: ApplicationContext) -> None:
+        await ctx.defer()
+        view = await self._get_set_trello_board_list_to_create_card_view(ctx)
+        await ctx.followup.send("用下拉選單設定新增卡片的位置", view=view, embed=view.embed)
+
+    @conf_tl.command(
+        name="board_keywords", description="設定 Trello 看板關鍵字"
+    )
+    @guild_only()
+    @has_any_role(*admin_roles)
+    async def set_board_keywords(self, ctx: ApplicationContext) -> None:
+        modal = await self._get_board_keyword_modal(ctx)
+        await ctx.send_modal(modal)
+
+    async def _get_set_trello_user_id_view(
+            self,
+            ctx: ApplicationContext,
+            trello: Optional[TrelloHandler] = None) -> SetTrelloUserIdView:
+        trello = trello or await get_trello_instance(self, ctx)
         # Update guild member list
         await self.bot.db.configure_guild_members(ctx)
         member_list = await self.bot.db.get_member_data(ctx.guild_id)
@@ -84,60 +124,49 @@ class Configuration(Cog):
 
         trello_id_to_name_dict["None"] = "None"
         is_set_callback = lambda discord_id, trello_id: self.bot.db.update_trello_id(ctx.guild_id, discord_id, trello_id)
-        view = SetTrelloUserIdView(
+        return SetTrelloUserIdView(
             ctx,
             members_in_guild_to_be_assigned,
             trello_id_to_name_dict,
             is_set_callback,
             discord_name_to_trello_name_dict)
-        await ctx.followup.send("用下拉選單設定成員名稱對照", view=view, embed=view.embed)
 
-    @conf_tl.command(
-        name="list_to_trace", description="Set interested Trello lists"
-    )
-    @guild_only()
-    @has_any_role(*admin_roles)
-    async def set_trello_intersted_lists(self, ctx: ApplicationContext) -> None:
-        await ctx.defer()
-        trello = await get_trello_instance(self, ctx)
+
+    async def _get_set_trello_intersted_lists_view(
+            self,
+            ctx: ApplicationContext,
+            trello: Optional[TrelloHandler] = None,
+            board_list_data: Optional[BoardListData] = None,
+            trello_settings: Optional[TrelloSettings] = None) -> SetTrelloTargetListView:
+        trello = trello or await get_trello_instance(self, ctx)
         if trello is None: return
+        board_list_data = board_list_data or await self.bot.trello.get_board_list_data(ctx.guild_id)
+        trello_settings = trello_settings or await self.bot.db.get_trello_settings(ctx.guild_id)
+        return SetTrelloTargetListView(ctx, board_list_data, trello_settings, self.bot.db)
 
-        board_list_data = await self.bot.trello.get_board_list_data(ctx.guild_id)
-        trello_settings = await self.bot.db.get_trello_settings(ctx.guild_id)
-
-        view = SetTrelloTargetListView(ctx, board_list_data, trello_settings, self.bot.db)
-        await ctx.followup.send("用下拉選單設定追蹤的看板", view=view, embed=view.embed)
-
-    @conf_tl.command(
-        name="board_entry", description="Set Trello list as board entry"
-    )
-    @guild_only()
-    @has_any_role(*admin_roles)
-    async def set_trello_board_entry(self, ctx: ApplicationContext) -> None:
-        await ctx.defer()
-        trello = await get_trello_instance(self, ctx)
+    async def _get_set_trello_board_list_to_create_card_view(
+            self,
+            ctx: ApplicationContext,
+            trello: Optional[TrelloHandler] = None,
+            board_list_data: Optional[BoardListData] = None,
+            trello_settings: Optional[TrelloSettings] = None) -> SetTrelloBoardListToCreateCardView:
+        trello = trello or await get_trello_instance(self, ctx)
         if trello is None: return
+        board_list_data = board_list_data or await self.bot.trello.get_board_list_data(ctx.guild_id)
+        trello_settings = trello_settings or await self.bot.db.get_trello_settings(ctx.guild_id)
+        return SetTrelloBoardListToCreateCardView(ctx, board_list_data, trello_settings, self.bot.db)
 
-        board_list_data = await self.bot.trello.get_board_list_data(ctx.guild_id)
-        trello_settings = await self.bot.db.get_trello_settings(ctx.guild_id)
-
-        view = SetTrelloBoardListToCreateCardView(ctx, board_list_data, trello_settings, self.bot.db)
-        await ctx.followup.send("用下拉選單設定新增卡片的位置", view=view, embed=view.embed)
-
-    @conf_tl.command(
-        name="board_keywords", description="Set Trello board keywords"
-    )
-    @guild_only()
-    @has_any_role(*admin_roles)
-    async def set_board_keywords(self, ctx: ApplicationContext) -> None:
-        trello = await get_trello_instance(self, ctx)
+    async def _get_board_keyword_modal(
+            self,
+            ctx: ApplicationContext,
+            trello: Optional[TrelloHandler] = None,
+            board_list_data: Optional[BoardListData] = None,
+            trello_settings: Optional[TrelloSettings] = None) -> BoardKeywordModal:
+        trello = trello or await get_trello_instance(self, ctx)
         if trello is None: return
-
-        board_list_data = await self.bot.trello.get_board_list_data(ctx.guild_id)
-        trello_settings = await self.bot.db.get_trello_settings(ctx.guild_id)
-
-        modal = BoardKeywordModal(ctx, board_list_data, trello_settings, self.bot.db)
-        await ctx.send_modal(modal)
+        board_list_data = board_list_data or await self.bot.trello.get_board_list_data(ctx.guild_id)
+        trello_settings = trello_settings or await self.bot.db.get_trello_settings(ctx.guild_id)
+        return BoardKeywordModal(ctx, board_list_data, trello_settings, self.bot.db)
 
 
 def setup(bot: Bot):
